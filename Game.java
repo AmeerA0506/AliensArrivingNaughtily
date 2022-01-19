c987be835236867d14a1fd2f76c0288f08d38b30
nakibabedin <nabedin30@stuy.edu>
2022-01-19 08:39:28 -0500

    update Woo.java

--- Game.java
+++ Game.java
@@ -10,15 +10,15 @@ public class Game {
   public static void initialScreen(){
     String initialScreen = "";
     initialScreen += "\033[1;36m_______";
-    initialScreen += "	\033[1;36m\n|/  |          ,--.           |";
-    initialScreen += "	\033[1;36m\n|  (_)         |   |,---.,---.|---";
-    initialScreen += "	\033[1;36m\n| \\ | /        |   ||   ||   ||";
-    initialScreen += "	\033[1;36m\n|  \\|/         `--' `---'`   '`---";
-    initialScreen += "	\033[1;36m\n|   |          |   |                   ,--.          ,-.-.";
-    initialScreen += "	\033[1;36m\n|   |          |---|,---.,---.,---.    |   |,---.    | | |,---.,---.";
-    initialScreen += "	\033[1;36m\n|  / \\         |   |,---||   ||   |    |   |,---|    | | |,---||   |";
-    initialScreen += "	\033[1;36m\n| /   \\        `   '`---^`   '`---|    `--' `---^    ` ' '`---''   '";
-    initialScreen += "	\033[1;36m\n|___|___                      `---'";
+    initialScreen += "\n|/  |          ,--.           |";
+    initialScreen += "\n|  (_)         |   |,---.,---.|---";
+    initialScreen += "\n| \\ | /        |   ||   ||   ||";
+    initialScreen += "\n|  \\|/         `--' `---'`   '`---";
+    initialScreen += "\n|   |          |   |                   ,--.          ,-.-.";
+    initialScreen += "\n|   |          |---|,---.,---.,---.    |   |,---.    | | |,---.,---.";
+    initialScreen += "\n|  / \\         |   |,---||   ||   |    |   |,---|    | | |,---||   |";
+    initialScreen += "\n| /   \\        `   '`---^`   '`---|    `--' `---^    ` ' '`---''   '";
+    initialScreen += "\n|___|___                      `---'";
     System.out.println(initialScreen);
 
   }
